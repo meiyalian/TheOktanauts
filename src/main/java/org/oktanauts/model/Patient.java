e6085467e6db711c67e24a4fa83334f79d5c3a73
meiyalian <mlia0004@student.monash.edu>
2020-05-13 12:13:49 +1000

    fix concurrence issues

--- src/main/java/org/oktanauts/model/Patient.java
+++ src/main/java/org/oktanauts/model/Patient.java
@@ -52,11 +52,17 @@ public class Patient {
         return id;
     }
 
-    public String getName() { return getFirstName() + " " + getSurname(); }
+    public String getName() {
+        return getFirstName() + " " + getSurname();
+    }
 
-    public String getFirstName() { return firstName; }
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public String getSurname() { return surname; }
+    public String getSurname() {
+        return surname;
+    }
 
     public Date getBirthday() {
         return birthday;
@@ -78,36 +84,31 @@ public class Patient {
         return country;
     }
 
-    public String getAddress(){
+    public String getAddress() {
         return this.city + ", " + this.state + ", " + this.country;
     }
 
-    public Boolean getHasWarning() { return hasWarning; }
+    public Boolean getHasWarning() {
+        return hasWarning;
+    }
 
-    public void setHasWarning(boolean hasWarning) { this.hasWarning = hasWarning; }
+    public void setHasWarning(boolean hasWarning) {
+        this.hasWarning = hasWarning;
+    }
 
-    public BooleanProperty selectedProperty(){
+    public BooleanProperty selectedProperty() {
         return isMonitored;
     }
 
-    public boolean isSelected(){
+    public boolean isSelected() {
         return isMonitored.get();
     }
 
-//    private static String readAll(Reader rd) throws IOException {
-//        StringBuilder sb = new StringBuilder();
-//        int cp;
-//        while ((cp = rd.read()) != -1) {
-//            sb.append((char) cp);
-//        }
-//        return sb.toString();
-//    }
 
 
-public void addMeasurement(String measurementCode, Measurement measurement){
-    measurements.put(measurementCode,measurement);
-}
-
+    public void addMeasurement(String measurementCode, Measurement measurement) {
+        measurements.put(measurementCode, measurement);
+    }
 
     public Measurement getMeasurement(String code) {
         if (!measurements.containsKey(code)) {
@@ -116,133 +117,5 @@ public void addMeasurement(String measurementCode, Measurement measurement){
         return measurements.get(code);
     }
 
-    /* error handling --when measurement doesn't exist should call callback with null
-        parameter and put null in hashmap */
-    public void updateMeasurement(String code, GetMeasurementCallback callback) {
-        String url = "https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/Observation?subject=" + this.id
-                + "&code=" + code + "&_format=json";
-
-        System.out.println(url);
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-
-            if (json.has("entry")){
-                JSONObject valueQuantity = json.getJSONArray("entry").getJSONObject(0).getJSONObject("resource")
-                        .getJSONObject("valueQuantity");
-
-                Timestamp dateTime = new Timestamp(new SimpleDateFormat("yyyy-MM-dd'T'H:m:s.SX")
-                        .parse(json.getJSONArray("entry").getJSONObject(0).getJSONObject("resource")
-                                .getString("issued")).getTime());
-                double value = valueQuantity.getDouble(  "value");
-                String unit = valueQuantity.getString("unit");
-                String name = json.getJSONArray("entry").getJSONObject(0).getJSONObject("resource")
-                        .getJSONObject("code").getString("text");
-
-                Measurement result = new Measurement(code, name, value, unit, dateTime, this);
-                measurements.put(code, result);
-            }
-            else{
-                measurements.put(code, null);
-                System.out.println("patient" + this.getName() + " doesnt have this measurement");
-            }
-
-            if (callback != null) {
-                callback.updateView();
-            }
-//
-//        }
-//        catch (IOException | ParseException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public Measurement getMeasurement(String code) {
-//        if (!measurements.containsKey(code)) {
-        //    updateMeasurement(code, null);
-//        }
-//        return measurements.get(code);
-//    }
-
-
-    //for testing
-//    public void updateMeasurementTesting(String code, GetMeasurementCallback callback){
-//        if (this.id == "1"){
-//            if(testUpdate == 0){
-//                Measurement m = new Measurement(code,"cholesterol level", 5.0f, "mg/dL", new Timestamp(2020,1,1,1,5,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//            if(testUpdate == 1){
-//
-//                Measurement m = new Measurement(code,"cholesterol level", 5.0f, "mg/dL", new Timestamp(2020,1,1,1,6,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//            if(testUpdate == 2){
-//                Measurement m = new Measurement(code,"cholesterol level", 8.0f, "mg/dL", new Timestamp(2020,1,1,1,7,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//            if(testUpdate == 3){
-//                Measurement m = new Measurement(code,"cholesterol level", 8.0f, "mg/dL", new Timestamp(2020,1,1,1,8,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//
-//        }
-//        if (this.id == "2"){
-//            if(testUpdate == 0){
-//                Measurement m = new Measurement(code,"cholesterol level", 5.0f, "mg/dL", new Timestamp(2020,1,1,1,5,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//            if(testUpdate == 1){
-//                Measurement m = new Measurement(code,"cholesterol level", 5.0f, "mg/dL", new Timestamp(2020,1,1,1,6,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//            if(testUpdate == 2){
-//                Measurement m = new Measurement(code,"cholesterol level", 5.0f, "mg/dL", new Timestamp(2020,1,1,1,7,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//            if(testUpdate == 3){
-//                Measurement m = new Measurement(code,"cholesterol level", 10.0f, "mg/dL", new Timestamp(2020,1,1,1,8,1,0), this );
-//                measurements.put(code, null);
-//
-//            }
-//
-//
-//        }
-//        if (this.id == "3"){
-//            if(testUpdate == 0){
-//                measurements.put(code, null);
-//            }
-//            if(testUpdate == 1){
-//                measurements.put(code, null);
-//            }
-//            if(testUpdate == 2){
-//                Measurement m = new Measurement(code,"cholesterol level", 5.0f, "mg/dL", new Timestamp(2020,1,1,1,5,1,0), this );
-//                measurements.put(code, null);
-//            }
-//            if(testUpdate == 3){
-//                Measurement m = new Measurement(code,"cholesterol level", 6.0f, "mg/dL", new Timestamp(2020,1,1,1,10,1,0), this );
-//                measurements.put(code, null);
-//            }
-//
-//        }
-//
-//        testUpdate +=1;
-//
-//    }
-//
-//    public Measurement getMeasurementTesting(String code) {
-//        if (!measurements.containsKey(code)) {
-//            updateMeasurementTesting(code, null);
-//        }
-//        return measurements.get(code);
-//    }
 
 }
